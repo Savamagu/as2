c571f232f43bf9dab777020760bf118e257be5e5
Savamagu <200239555@BA-LC2-1808.student.georgianc.on.ca>
2015-04-09 18:40:13 -0400

    app.js

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// Saving Images
-var formidable = require('formidable');
-var util = require('util');
-var fs = require('fs-extra');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// File uploading
+var formidable = require('formidable');
+var util = require('util');
+var fs = require('fs-extra');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,14 +29,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+//app.use(auth);
 
 // Database
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/as2');
+mongoose.connect('mongodb://as2:as2@ds061601.mongolab.com:61601/heroku');
 
 // Routing
 var printers = require('./routes/printers');
 app.use('/', printers);
+
+// Authentication
+/* app.use(basicAuth('admin', 'admin'));
+app.use(basicAuth(function(user, pass, fn) {
+	var result = (user === 'admin' && pass === 'admin');
+	
+	if (result) fn(null, user);
+	else fn(err, user);
+})); */
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
